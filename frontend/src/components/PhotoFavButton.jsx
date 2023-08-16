db9e9228e3daa0c52a023cbc1794debc4705f652
Sandeep Gill <sandeepgill62@gmail.com>
2023-08-16 03:22:46 +0000

    Add the Favourite Icon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,16 +1,30 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+
+  const [selected, setSelected] = useState(false);
+
+  const onClick = () => {
+    if (selected) {
+      setSelected(false);
+    } else {
+      setSelected(true);
+    }
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon
+          onClick={onClick}
+          selected={selected}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
+//
+import '../styles/PhotoList.scss';
+
+//
+
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
 
   const { item, key } = props;
 
   return (
-    <div className="photo-list__item">
-      <img className="photo-list__image" src={item.imageSource} alt="image" />
-      <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={item.profile} alt="" />
-        <div>
-          <p className="photo-list__user-info">{item.username}</p>
-          <p className="photo-list__user-location">{item.location.city} {item.location.country}</p>
+    <div className="photo-list">
+      <div className="photo-list__item">
+        <PhotoFavButton />
+        <img className="photo-list__image" src={item.imageSource} alt="image" />
+
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={item.profile} alt="" />
+          <div>
+            <p className="photo-list__user-info">{item.username}</p>
+            <p className="photo-list__user-location">{item.location.city} {item.location.country}</p>
+          </div>
         </div>
-      </div>
-    </div >
+      </div >
+    </div>
   );
 };
 
